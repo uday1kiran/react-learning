1ff677032831c388182f74af0d70642b6d17ad8c
uday <uday1kiran@gmail.com>
2023-11-05 20:22:04 +0530

    separate function for event handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,17 +3,15 @@ function ListGroup() {
   //items = [];
   //const message = items.length === 0 ? <p>No item found</p> : null;
   const message = items.length === 0 && <p>No item found in list</p>;
+
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {message}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={(event) => console.log("Clicked", item, index, event)}
-          >
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
